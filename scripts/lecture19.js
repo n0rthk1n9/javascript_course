ba93906cdb970f3007d1321c475ae8bee1474767
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-11 19:16:08 +0200

    initial commit of lecture 19

---
+++ scripts/lecture19.js
@@ -0,0 +1,9 @@
+// cover all functions in on eventhandler function
+function eventHandler() {
+  
+}
+
+// eventhandler fucntion is called on pageload
+window.onload = function() {
+  eventHandler();
+}
