b68a510049d22d57dcf14a2d15ff672fcc3f20f4
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-11 13:44:28 +0200

    cover all functions in eventhandler function and call it on page load

--- scripts/lecture17.js
+++ scripts/lecture17.js
@@ -1,22 +1,30 @@
-// save content div in array and change alignment attribute
-var content = document.getElementById('content');
-content.setAttribute('align', 'right');
+// cover all functions in on eventhandler function
+function eventHandler() {
+  // save content div in array and change alignment attribute
+  var content = document.getElementById('content');
+  content.setAttribute('align', 'right');
 
-// define onclick function of button
-document.getElementById('changeAlignButton').onclick = function changeAlignmentOfContent() {
-  // change description of button and set alignment attributes
-  if (document.getElementById('changeAlignButton').innerHTML == "Ausrichtung rechts") {
-    document.getElementById('changeAlignButton').innerHTML = "Ausrichtung links"
-    content.setAttribute('align', 'right');
-  } else {
-    document.getElementById('changeAlignButton').innerHTML = "Ausrichtung rechts"
-    content.setAttribute('align', 'left');
+  // define onclick function of button
+  document.getElementById('changeAlignButton').onclick = function changeAlignmentOfContent() {
+    // change description of button and set alignment attributes
+    if (document.getElementById('changeAlignButton').innerHTML == "Ausrichtung rechts") {
+      document.getElementById('changeAlignButton').innerHTML = "Ausrichtung links"
+      content.setAttribute('align', 'right');
+    } else {
+      document.getElementById('changeAlignButton').innerHTML = "Ausrichtung rechts"
+      content.setAttribute('align', 'left');
+    }
+  }
+
+  // on click on button a new p tag is created and appended to div with id content
+  document.getElementById('createNewDomElement').onclick = function createNewDomElement() {
+    var newPTag = document.createElement('p');
+    newPTag.innerHTML = 'Ich bin dynamisch aus JavaScript heraus erzeugt!';
+    content.appendChild(newPTag);
   }
 }
 
-// on click on button a new p tag is created and appended to div with id content
-document.getElementById('createNewDomElement').onclick = function createNewDomElement() {
-  var newPTag = document.createElement('p');
-  newPTag.innerHTML = 'Ich bin dynamisch aus JavaScript heraus erzeugt!';
-  content.appendChild(newPTag);
+// eventhandler fucntion is called on pageload
+window.onload = function() {
+  eventHandler();
 }
