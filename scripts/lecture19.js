cddfdb87a121883b8813fbde92284e916bdfdf98
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-11 19:59:43 +0200

    show and hide part of a form when checking checkbox

--- scripts/lecture19.js
+++ scripts/lecture19.js
@@ -1,9 +1,18 @@
-// cover all functions in on eventhandler function
-function eventHandler() {
-  
+// show and hide the full adress div of the form when checking the checkbox
+function showFullAdress() {
+  document.getElementById('fullAdressCheckbox').onclick = function () {
+    if (document.getElementById('fullAdressCheckbox').checked) {
+      document.getElementById('fullAdress').style.display = "block";
+    } else {
+      document.getElementById('fullAdress').style.display = "none";
+    }
+  };
 }
 
-// eventhandler fucntion is called on pageload
+// hide the full adress div at initial page load
+document.getElementById('fullAdress').style.display = "none";
+
+// showFullAdress fucntion is called on pageload
 window.onload = function() {
-  eventHandler();
+  showFullAdress();
 }
