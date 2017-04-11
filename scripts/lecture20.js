73ac52f3ceb727f0138b865c9c162c9e081430ef
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-11 20:37:34 +0200

    set a css class to an DOM Element and remove it

--- scripts/lecture20.js
+++ scripts/lecture20.js
@@ -1,6 +1,13 @@
 // set new class property to div tag content
 function showSetClass() {
-
+  document.getElementById('content').onclick = function setClass() {
+    if (document.getElementById('content').className == "example") {
+      // remove specific className from list of class names
+      document.getElementById('content').classList.remove("example");
+    } else {
+      document.getElementById('content').className = "example";
+    }
+  }
 }
 
 // showSetClass fucntion is called on pageload
